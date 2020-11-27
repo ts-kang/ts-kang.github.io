c4193e03236952e9d58fd3118ed1be56819dfa02
Taesu Kang <null@ieee.org>
2020-11-27 11:58:57 +0900

    .

--- ereter-dp/js/script.js
+++ ereter-dp/js/script.js
@@ -2,17 +2,23 @@ const url_ereter_analytics = 'http://ereter.net/iidxplayerdata/{}/analytics/perl
 const url_ereter_level = 'http://ereter.net/iidxplayerdata/{}/level/12/';
 
 const lamps = ['no-play', 'failed', 'assist', 'easy', 'clear', 'hard', 'ex-hard', 'fullcombo'];
+const ranks = ['F', 'F', 'E', 'D', 'C', 'B', 'A', 'AA', 'AAA'];
 
 const comparators = {
     title: (a, b) => a.title.localeCompare(b.title),
     clear_lamp: (a, b) => lamps.indexOf(a.lamp) - lamps.indexOf(b.lamp),
     ereter_difficulty: (a, b) => a.recommend[0] - b.recommend[0],
-    rank: (a, b) => a.rank - b.rank
+    rank: (a, b) => ranks.indexOf(a.rank) - ranks.indexOf(b.rank),
+    percentage: (a, b) => a.percentage - b.percentage
 }
 
 var config = {
     show: {
-        ereter_color: true
+        title: true,
+        rank: false,
+        percentage: false,
+        clear_lamp: true,
+        ereter_color: false
     }
 }
 
@@ -24,8 +30,6 @@ $(document).ready(async () => {
         return false;
     });
 
-    var diff_table = {};
-
     let player = $('#player').val();
 
     if (!player)
@@ -36,6 +40,15 @@ $(document).ready(async () => {
     var diff_table = [];
     var index_table = {}; // song_id: [level_index, song_index]
     var user_info = '';
+
+    Object.keys(config.show).forEach((opt) => {
+        $('#show_' + opt).on('click', function () {
+            config.show[opt] = !config.show[opt];
+            config_update();
+            table_render(diff_table, user_info);
+        });
+    });
+    config_update();
 
     content.html('<h3 class="loading">Loading... (1/2)</h3>');
     await access_page(url_ereter_analytics.replace('{}', player), (data) => {
@@ -55,62 +68,34 @@ $(document).ready(async () => {
         table_parse_records(diff_table, records_table, index_table);
     });
 
-    diff_table.forEach((level_table) => {
-        let recommend_sum = level_table.songs
-            .reduce((total, current) => {
-                total.recommend = current.recommend
-                    .map((rec, i) => total.recommend[i] + rec);
-                total.recommend_color = current.recommend_color
-                    .map((color, i) =>
-                         color.map((c, j) => total.recommend_color[i][j] + c));
-                return total;
-            }, {
-                recommend: [0, 0, 0],
-                recommend_color: [0, 0, 0].map(() => [0, 0, 0])
-            });
-
-        level_table.recommend =
-            recommend_sum.recommend.map((rec) => Math.floor(rec / level_table.songs.length * 10.0) / 10.0);
-        level_table.recommend_color =
-            recommend_sum.recommend_color.map((color) => color.map((c) => parseInt(c / level_table.songs.length)));
-
-        level_table.lamp = level_table.songs
-            .reduce((total, current) =>
-                    lamps.indexOf(current.lamp) < lamps.indexOf(total.lamp) ? current : total,
-                    {lamp: lamps[lamps.length - 1]}).lamp;
-    });
     diff_table.reverse();
 
     $('#screenshot').on('click', () => {
+        $('#download_screenshot').remove();
         window.scrollTo(0,0);
         html2canvas($('#content')[0], { backgroundColor: '#252830' }).then((canvas) => {
             let a = document.createElement('a');
-            a.download = 'user_' + player + '_' + new Date().toISOString().replace(/^([\d-]+)[\w][\d:.]+[\w]$/, '$1') + '.png';
-            a.href = canvas.toDataURL('image/png');
-            a.click();
-            a.innerText = 'Download screenshot';
-            $(a).insertAfter('#screenshot');
+            a.id = 'download_screenshot';
+            canvas.toBlob((blob) => {
+                a.href = URL.createObjectURL(blob);
+                a.download = 'user_' + player + '_' + new Date().toISOString().replace(/^([\d-]+)[\w][\d:.]+[\w]$/, '$1') + '.png';
+                a.click();
+                a.innerText = 'Download screenshot';
+                $(a).insertAfter('#screenshot');
+            });
         });
     });
-
-    Object.keys(config.show).forEach((opt) => {
-        $('#show_' + opt).on('click', function () {
-            config.show[opt] = !config.show[opt];
-            config_update();
-            table_render(diff_table, user_info);
-        });
-    })
 
     Object.keys(comparators).forEach((comp) => {
         $('#sort_' + comp).on('click', function () {
             var status = $(this).hasClass('sort-up') ? 1 :
                 $(this).hasClass('sort-down') ? -1 : 0;
-            $(this).removeClass('sort-up sort-down');
+            $(this).removeClass('sort-up sort-down enabled');
             status = (status % 3 + 3) % 3 - 1;
             if (status == 1)
-                $(this).addClass('sort-up');
+                $(this).addClass('sort-up enabled');
             else if (status == -1)
-                $(this).addClass('sort-down');
+                $(this).addClass('sort-down enabled');
             else
                 return;
 
@@ -119,7 +104,6 @@ $(document).ready(async () => {
         });
     });
 
-    config_update();
     $('#sort_ereter_difficulty').click();
     $('#sort_clear_lamp').click();
 });
@@ -143,26 +127,39 @@ function config_update() {
     Object.keys(config).forEach((conf) => {
         Object.keys(config[conf]).forEach((opt) => {
             if (config[conf][opt])
-                $(`#${conf}_${opt}`).css('color', '#1997c6');
+                $(`#${conf}_${opt}`).addClass('enabled');
             else
-                $(`#${conf}_${opt}`).css('color', '');
+                $(`#${conf}_${opt}`).removeClass('enabled');
         });
     });
 }
 
+function config_(conf, str, def) {
+    return conf ? str : def == undefined ? '' : def;
+}
+
 function table_render(diff_table, user_info) {
+    if (!diff_table || !user_info)
+        return;
+    
     let table_object = diff_table.map((level_table) => {
         let songs = level_table.songs
-            .map((song) => `
-                <div class="song"` +
-                 (config.show.ereter_color
-                  ? ` style="background-color: rgba(${song.recommend_color[0].join(', ')}, 0.3)"`
-                  : '')
-                + `>
-                    <span class="title ${song.difficulty.toLowerCase()}">${song.title}</span>
-                    <span class="lamp ${song.lamp}"></span>
-                </div>
-            `)
+            .map((song) => '<div class="song"' +
+                 config_(config.show.ereter_color,
+                         ` style="background-color: rgba(${song.recommend_color[0].join(', ')}, 0.3)"`) +
+                 /*config_(config.show.ereter_color,
+                         ` style="background-image: linear-gradient(to right, rgba(${song.recommend_color[0].join(', ')}, 0.3) 25%, rgba(${song.recommend_color[0].join(', ')}, 0.1) 100%)"`) +*/
+                 '>' +
+                 config_(config.show.title,
+                         `<span class="title ${song.difficulty.toLowerCase()}">${song.title}</span>`) +
+                 '<div class="right">' +
+                 config_(config.show.rank,
+                         `<span class="rank ${song.rank.toLowerCase()}">${song.rank}</span>`) +
+                 config_(config.show.percentage,
+                         `<span class="rank ${song.rank.toLowerCase()}" style="transform: scale(0.9, 1)">${parseInt(song.percentage)}%</span>`) +
+                 config_(config.show.clear_lamp,
+                         `<span class="lamp ${song.lamp}"></span>`) +
+                 '</div></div>')
             .join('');
         let container = $(`
         <div class="container">
@@ -170,7 +167,16 @@ function table_render(diff_table, user_info) {
                 <span style="color: rgb(${level_table.recommend_color[0].join(', ')})">
                     ☆${level_table.level}
                 </span>
-                <span class="lamp ${level_table.lamp}"></span>
+                <div>` +
+                 config_(config.show.rank,
+                         `<span class="rank ${level_table.rank.toLowerCase()}">${level_table.rank}</span>`) +
+                 config_(config.show.percentage,
+                         `<span class="rank ${level_table.rank.toLowerCase()}">${parseInt(level_table.percentage)}%</span>`) +
+                `</div>
+                <span class="right">` +
+                 config_(config.show.clear_lamp,
+                         `<span class="lamp ${level_table.lamp}"></span>`) +
+                `</span>
             </div>
             <div class="songs">${songs}</div>
         </div>
@@ -183,11 +189,12 @@ function table_render(diff_table, user_info) {
         .html(`<h3 style="margin-top: 0">${user_info}</h3>`)
         .append(table_object);
 
-    let div_width = $('.song').width() - $('.song .lamp').width() * 1.5;
-    $('.song .title').each((_, title) => {
-        if ($(title).width() > div_width) {
-            let width_scale = Math.max(div_width / $(title).width(), 0.75);
-            $(title).css('transform', `scale(${width_scale}, 1)`);
+    $('.song').each((_, song) => {
+        let title = $(song).find('.title');
+        let div_width = $(song).width() - ($($(song).find('.right')).width() || 0) - 3;
+        if (title.width() > div_width) {
+            let width_scale = Math.max(div_width / title.width(), 0.5);
+            title.css('transform', `scale(${width_scale}, 1)`);
         }
     });
 }
@@ -250,9 +257,44 @@ function table_parse_records(diff_table, records_table, index_table) {
             song.rank = $(fields[4]).children().length > 0
                 ? $(fields[4]).find('span span span')[0].innerText
                 : '';
+            song.percentage = $(fields[4]).children().length > 0
+                ? parseFloat($(fields[4]).find('span span span')[1].innerText)
+                : 0;
             song.lamp = $(fields[5]).text().toLowerCase();
             if (song.lamp.trim() === '')
                 song.lamp = 'no-play';
         });
+    });
+
+    diff_table.forEach((level_table) => {
+        var size = level_table.songs.length;
+
+        let level_sum = level_table.songs
+            .reduce((total, current) => {
+                if (current.rank === '')
+                    --size;
+                total.percentage += current.percentage;
+                total.recommend = current.recommend
+                    .map((rec, i) => total.recommend[i] + rec);
+                total.recommend_color = current.recommend_color
+                    .map((color, i) =>
+                         color.map((c, j) => total.recommend_color[i][j] + c));
+                return total;
+            }, {
+                percentage: 0,
+                recommend: [0, 0, 0],
+                recommend_color: [0, 0, 0].map(() => [0, 0, 0])
+            });
+
+        level_table.percentage = (level_sum.percentage / size) || 0;
+        level_table.rank = ranks[Math.trunc(level_table.percentage / 100 * 9)];
+        level_table.recommend =
+            level_sum.recommend.map((rec) => Math.floor(rec / level_table.songs.length * 10.0) / 10.0);
+        level_table.recommend_color =
+            level_sum.recommend_color.map((color) => color.map((c) => parseInt(c / level_table.songs.length)));
+        level_table.lamp = level_table.songs
+            .reduce((total, current) =>
+                    lamps.indexOf(current.lamp) < lamps.indexOf(total.lamp) ? current : total,
+                    {lamp: lamps[lamps.length - 1]}).lamp;
     });
 }
