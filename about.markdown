ccebf7c2cf13dfd02ea30c9e409b26375200b30c
Taesu Kang <null@ieee.org>
2022-01-11 16:59:47 +0900

    Setup jekyll

---
+++ about.markdown
@@ -0,0 +1,8 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+
+# 소개
+소프트웨어 전공 중인 학생입니다.
--- js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  document.body.style.backgroundColor = '#dddddd';
-
-  if(window.location.href.indexOf('http://') === 0)
-    window.location.href = window.location.href.replace('http://', 'https://');
-}
